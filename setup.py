5a8328d0e42737fdda71806f37e7277c429567cb
Hamza Mughal <mhamza.mughal09@gmail.com>
2022-08-08 15:27:35 +0200

    add package setup code with requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='conv_act',
+    packages=find_packages(exclude="configs"),
+    version='0.1.0',
+    description='Human Activity Recognition (HAR) with Transformer based on Convolutional Features.',
+    author='Hamza Mughal',
+    python_requires=">=3.6",
+    install_requires=[
+        "torch",
+        "torchvision",
+        "tqdm",
+        "numpy",
+        "pyyaml",
+        "av"
+    ],
+    license='MIT',
+)
